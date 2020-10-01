c37811f52fc40819416e8f335fa68ed9655e8b82
devsehyeon <sehyeonpark@indentcorp.com>
2020-10-01 16:55:38 +0900

    default reducer example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,26 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+const INCREMENT = 'increment'
+const DECREMENT = 'decrement'
+
+const reducer = (state: any, action: any) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 }
+    case DECREMENT:
+      return { count: state.count - 1 }
+    default:
+      throw new Error()
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, { count: 0 })
   return (
-    <div className="App">
-      <p>Hooks Todo</p>
+    <div>
+      <h1>{state.count}</h1>
+      <button onClick={() => dispatch({ type: INCREMENT })}>Plus</button>
+      <button onClick={() => dispatch({ type: DECREMENT })}>Minus</button>
     </div>
   )
 }
